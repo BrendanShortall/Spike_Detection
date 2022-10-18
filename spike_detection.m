044338fa11bce1f9c2bd5899f157599907b62a4e
BrendanShortall <115127093+BrendanShortall@users.noreply.github.com>
2022-10-18 16:08:48 -0400

    Update spike_detection.m

--- spike_detection.m
+++ spike_detection.m
@@ -1,12 +1,16 @@
 function [valid_peaks,peak_ind] = spike_detection(data,threshold)
+%Get local maxima/minima values
 [maxima, minima, min_ind, max_ind] = process(data);
 minima = -minima;
+%Find peaks that are less than the specified threshold
 invalid_peaks = find(maxima < threshold);
+%Remove the peaks that are invalid, or not above the threshold
 maxima(invalid_peaks)=[];
 max_ind(invalid_peaks)=[];
 invalid_below = find(minima > -threshold);
 minima(invalid_below) = [];
 min_ind(invalid_below) = [];
+%Return all valid peaks 
 valid_peaks = [maxima;minima];
 peak_ind = [max_ind;min_ind];
 end
