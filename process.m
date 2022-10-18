43c0e06c0f62036baef0f085ddb47ed148691959
BrendanShortall <115127093+BrendanShortall@users.noreply.github.com>
2022-10-18 16:09:34 -0400

    Update process.m

--- process.m
+++ process.m
@@ -1,4 +1,6 @@
 function [maxima, minima, min_ind, max_ind, amplitudes] = process(data)
+%Function returns the amplitutde and positons of local maxima and minima
+%using built in findpeaks function.
     [maxima, max_ind] = findpeaks(data);
     [minima, min_ind] = findpeaks(-data);
 %     num_peaks = size(max_ind, 1);
